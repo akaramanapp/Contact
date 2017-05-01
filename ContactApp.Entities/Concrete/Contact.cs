bee30d30f052b24e0e085d604c6cc6bfb358d90c
abdulkerimkaraman <abdulkerimkaraman@gmail.com>
2017-05-01 15:33:55 +0300

    Attribute

--- ContactApp.Entities/Concrete/Contact.cs
+++ ContactApp.Entities/Concrete/Contact.cs
@@ -1,13 +1,19 @@
 ﻿using ContactApp.Core.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContactApp.Entities.Concrete
 {
     public class Contact: IEntity
     {
         public int ContactId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
     }
 }
